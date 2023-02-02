582de1fd1efe3324c038b96630d0cb113337113f
Jhon Keneth Namias <75213409+PP-Namias@users.noreply.github.com>
2023-02-02 11:35:03 +0800

    :+1: Design Update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 const parent = document.querySelectorAll('.js-input');
 
-
 window.addEventListener('pageshow', () => {
     parent.forEach(e => {
 
@@ -28,17 +27,32 @@ function blurState(e){
     }
 }
 
-const passInput = document.querySelector('.js-pass')
-const showHidePass = document.querySelector('#show-hide-pass')
 
-showHidePass.addEventListener('click', () => {
-    if(passInput.type == "password"){
-        passInput.type = "text";
-        showHidePass.src = "assets/hidePass.svg"
-    }else{
-        passInput.type = "password";
-        showHidePass.src = "assets/showPass.svg"
-    }
+
+
+
+
+const wrapper = document.querySelector(".wrapper"),
+    qrInput = wrapper.querySelector(".form input"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value.trim();
+    if (!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active");
+        generateBtn.innerText = "Generate QR Code";
+    });
 });
 
-console.log(passInput);
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        wrapper.classList.remove("active");
+        preValue = "";
+    }
+});
